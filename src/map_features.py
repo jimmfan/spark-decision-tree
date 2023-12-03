4b6f89a50294e26d1a0f7a1e08c1421636ccfbf9
jimmfan <jimmfan@users.noreply.github.com>
2023-12-03 05:45:07 +0000

    renamed files. using different example

--- src/map_features.py
+++ src/map_features.py
@@ -85,7 +85,7 @@ df_sql_rules = (
 )
 
 df_sql_rules.show(20, False)
-
+df_sql_rules.show(20, False)
 csv_path = "./src/sample_data/data.csv"
 
 # Read Training Data in
@@ -97,7 +97,7 @@ training_data.createOrReplaceTempView("temp_data")
 query = """
 SELECT *
 FROM temp_data
-WHERE income > 49000.0 AND full_time > 0.5 AND credit_age > 39.5 AND credit_age > 43.5
+WHERE income > 49000.0 AND full_time > 0.5 AND credit_age < 39.5
 """
 
 filtered_df = spark.sql(query)
